f5967731c5c56a2677a6df35386b3fb275b3e29c
André Souza <aljs.junca@gmail.com>
2023-03-17 12:49:16 -0300

    Estudando Arrays

---
+++ tuplas.php
@@ -0,0 +1,8 @@
+<?php
+
+$dados = ['André',10,23];
+[$nome, $nota, $idade] = $dados;
+
+var_dump($nome);
+var_dump($nota);
+var_dump($idade);
--- uniaoArray.php
+++ uniaoArray.php
@@ -1,1 +1,25 @@
 <?php
+
+$alunos2022 = [
+    'Vinicius',
+    'Joao',
+    'Ana',
+    'Roberto',
+    'Maria',
+];
+
+$novosAlunos = [
+    'André',
+    'Julho',
+    'Athena',
+    'Angel',
+    'Mia',
+    'Antony',
+    'Litte Devil'
+];
+
+$alunos2023 = array_merge($alunos2022,$novosAlunos);
+
+sort($alunos2023);
+
+var_dump($alunos2023);
