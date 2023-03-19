2f3c98ce69e9aa7dc067aa377226d2800053ab57
André Souza <aljs.junca@gmail.com>
2023-03-19 10:54:40 -0300

    Estudo de Orientação a Objeto

--- ObjectOriented/Banco/src/Conta.php
+++ ObjectOriented/Banco/src/Conta.php
@@ -9,10 +9,26 @@ class Conta
     public function sacar($valor)
     {
         if($valor > $this->saldo) {
-            echo 'Não pode sacar valor maior que o Saldo';
+            echo "Conta do {$this->nomeTitular} Não pode sacar valor maior que o Saldo" . PHP_EOL;
         } else {
             $this->saldo -= $valor;
-            echo "Valor Sacado: {$valor}";
+            echo "Conta do {$this->nomeTitular}: Valor Sacado: {$valor}" . PHP_EOL;
         }
     }
+
+    public function depositar($valor) {
+        $this->saldo += $valor;
+        echo "Conta do {$this->nomeTitular}: Seu novo saldo é {$this->saldo}" . PHP_EOL;
+    }
+
+    public function transferir(Conta $conta, float $valor){
+        if ($valor > $this->saldo) {
+            echo "Conta do {$this->nomeTitular}: Você não pode transferir" . PHP_EOL;
+        } else {
+            $this->sacar($valor);
+            $conta->depositar($valor);
+            echo "Conta do {$this->nomeTitular}: valor R$ {$valor} Transferido para {$conta->nomeTitular}" . PHP_EOL;
+        }
+    }
+
 }
